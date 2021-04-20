4b41863f1a86f31a8c408636743f5fd1b879fee8
Copi5336 <jason.han@tpisoftware.com>
2021-04-20 17:24:37 +0800

    回家讀書

--- src/pages/contact/contactPage.js
+++ src/pages/contact/contactPage.js
@@ -4,7 +4,7 @@ import "./contact.scss";
 
 function ContactPage() {
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data) => {
     console.log(data);
   }
@@ -15,14 +15,17 @@ function ContactPage() {
         <div className="mb-3">
           <label htmlFor="exampleFormControlInput1" className="form-label">姓</label>
           <input  {...register("firstName", { required: true, maxLength: 20 })} type="text" className="form-control" data-id="exampleFormControlInput1" placeholder="呂"></input>
+          {errors.firstName && <span>This field is required</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="exampleFormControlInput2" className="form-label">名</label>
           <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} type="text" className="form-control" data-id="exampleFormControlInput2" placeholder="鳳仙"></input>
+          {errors.lastName && <span>This field is required</span>}
         </div>
         <div className="mb-3">
           <label htmlFor="exampleFormControlInput3" className="form-label">年紀</label>
           <input  {...register("age", { min: 18, max: 99 })} type="number" className="form-control" data-id="exampleFormControlInput3" placeholder="0"></input>
+          {errors.age && <span>This field is required</span>}
         </div>
         <input type="submit" />
       </form>
